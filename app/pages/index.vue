a6329ac9d1ae9059a09e50ef7b8b655416bc63b2
Deri Prasetyo <deri.mbois@gmail.com>
2026-01-28 16:40:46 +0700

    feat: add income and expense calculations to dashboard and improve transaction pagination

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,4 +1,21 @@
 <script setup lang="ts">
+import { useObservable } from '@vueuse/rxjs';
+import { from } from 'rxjs';
+import { liveQuery } from 'dexie';
+
+const { db } = useDb();
+const totalIncome = useObservable(from(liveQuery(async () => {
+  const income = await db.transactions.where('type').equals('1').toArray();
+  return income.reduce((sum, trx) => sum + Number(trx.amount), 0);
+})));
+const totalExpense = useObservable(from(liveQuery(async () => {
+  const income = await db.transactions.where('type').equals('2').toArray();
+  return income.reduce((sum, trx) => sum + Number(trx.amount), 0);
+})));
+const saldo = computed(() => {
+  return (totalIncome.value || 0) - (totalExpense.value || 0);
+});
+
 useHead({
   title: 'Notely - Dashboard',
 });
@@ -6,19 +23,40 @@ useHead({
 
 <template>
   <NuxtLayout name="default" title="Dashboard">
-    <UAlert color="neutral" variant="subtle" title="Mulai mencatat!"
-      description="Jangan lupa mencatat setiap hari."
-      icon="i-lucide-rocket"
-      orientation="horizontal"
-      :actions="[
+    <UAlert color="neutral" variant="subtle" title="Mulai mencatat!" description="Jangan lupa mencatat setiap hari."
+      icon="i-lucide-rocket" orientation="horizontal" :actions="[
         {
           label: 'Mulai!',
           to: '/transaction'
         }
-      ]"
-      :ui="{
+      ]" :ui="{
         icon: 'size-10',
-      }"
-       />
+      }" />
+    <div class="grid space-y-4">
+      <UCard>
+        <template #header>
+          <p class="font-bold text-lg">Total Pemasukan</p>
+        </template>
+        <template #default>
+          <p class="font-bold text-xl">{{ Number(totalIncome || 0).toLocaleString('id-ID', { style: 'currency', currency: 'IDR' }) }}</p>
+        </template>
+      </UCard>
+      <UCard>
+        <template #header>
+          <p class="font-bold text-lg">Total Pengeluaran</p>
+        </template>
+        <template #default>
+          <p class="font-bold text-xl">{{ Number(totalExpense || 0).toLocaleString('id-ID', { style: 'currency', currency: 'IDR' }) }}</p>
+        </template>
+      </UCard>
+      <UCard>
+        <template #header>
+          <p class="font-bold text-lg">Saldo saat ini</p>
+        </template>
+        <template #default>
+          <p class="font-bold text-xl">{{ Number(saldo || 0).toLocaleString('id-ID', { style: 'currency', currency: 'IDR' }) }}</p>
+        </template>
+      </UCard>
+    </div>
   </NuxtLayout>
 </template>
--- app/pages/transaction.vue
+++ app/pages/transaction.vue
@@ -12,9 +12,10 @@ const totalPage = computed(() => Math.ceil((totalTransactions.value || 0) / 5));
 const currentPage = useState<number>('transaction-current-page', () => 1);
 let transactions = useObservable(from(liveQuery(() => db.transactions.offset((currentPage.value - 1) * 5).limit(5).toArray())));
 
+
 watch(currentPage, (newValue) => {
   // Trigger re-fetching transactions when the current page changes
-  transactions = useObservable(from(liveQuery(() => db.transactions.offset((newValue - 1) * 5).limit(5).toArray())));  
+  transactions = useObservable(from(liveQuery(() => db.transactions.offset((newValue - 1) * 5).limit(5).toArray())));
 });
 </script>
 
@@ -29,10 +30,12 @@ watch(currentPage, (newValue) => {
       <p class="text-sm text-muted">{{ new Date(trx.date).toLocaleDateString() }}</p>
       <p class="text-lg font-bold mt-2">{{ Number(trx.amount).toLocaleString('id-ID', {
         style: 'currency', currency:
-          'IDR' }) }}</p>
+          'IDR'
+      }) }}</p>
     </div>
     <div id="pagination" class="flex justify-center">
-      <UPagination v-if="totalPage > 1" v-model:page="currentPage" :total="totalTransactions" :items-per-page="5" class="my-4" />
+      <UPagination v-if="totalPage > 1" v-model:page="currentPage" :total="totalTransactions" :items-per-page="5"
+        class="my-4" />
     </div>
     <UButton icon="i-lucide-plus" class="fixed bottom-6 right-6 rounded-full" size="xl" @click="isOpen = true" />
 
